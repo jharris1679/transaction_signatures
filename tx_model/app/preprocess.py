81210406348f8fa4ebe530785bb13c3608dd7b1a
jharris1679 <jdharris1357@gmail.com>
2020-05-03 13:31:13 -0400

    move parallelized funtion out of main object, move config to file

--- tx_model/app/preprocess.py
+++ tx_model/app/preprocess.py
@@ -11,6 +11,7 @@ import multiprocessing as multi
 import time
 import math
 import json
+import tempfile
 
 class BigQuery(object):
     def __init__(self, sample_size, isCached=False, isLocal=False):
@@ -154,29 +155,36 @@ class Dictionary(object):
 
 
 class Features(object):
-    def __init__(self,
-                 dataset_name,
-                 seq_len,
-                 sample_size,
-                 feature_set,
-                 isCached=False,
-                 isLocal=False):
-        bq = BigQuery(sample_size, isCached, isLocal)
-        self.dataset_name = dataset_name
-        self.seq_len = seq_len
-        self.feature_set = feature_set
+    def __init__(self, args, isCached=False, isLocal=False):
+        self.args = args
+
         self.dictionary = Dictionary()
         self.dictionary.add_merchant('<cls>')
         self.dictionary.add_merchant('<pad>')
         self.dictionary.add_merchant('<unk>')
 
-        if sample_size==-1:
+        bq = BigQuery(args.sample_size, isCached, isLocal)
+
+        with open('feature_config.json', 'r') as config:
+            self.feature_config = json.load(config)
+        assert self.feature_config['merchant_name']['enabled'] == True
+
+        # Use args to enable features
+        self.feature_config['user_reference']['enabled'] = self.args.include_user_context
+        self.feature_config['eighth_of_day']['enabled'] = self.args.include_eighth_of_day
+        self.feature_config['day_of_week']['enabled'] = self.args.include_day_of_week
+        self.feature_config['amount']['enabled'] = self.args.include_amount
+        self.feature_config['sys_category']['enabled'] = self.args.include_sys_category
+
+        # Create directories
+        if args.sample_size==-1:
             sample_dir = 'sample_all'
         else:
-            sample_dir = 'sample_{0}'.format(str(sample_size))
-        dataset_dir  = '{0}_{1}_data'.format(self.dataset_name, str(self.seq_len))
+            sample_dir = 'sample_{0}'.format(str(args.sample_size))
+        dataset_dir  = '{0}_{1}_data'.format(self.args.dataset_name, str(self.args.seq_len))
         self.data_dir = os.path.join(sample_dir, dataset_dir)
         try:
+            os.mkdir('tmp_data')
             os.mkdir(sample_dir)
             os.mkdir(self.data_dir)
         except FileExistsError:
@@ -184,7 +192,7 @@ class Features(object):
 
         # Load pretrained embeddings
         # First two colums are index and merchant_name
-        if dataset_name=='merchant_seqs_by_tx':
+        if args.dataset_name=='merchant_seqs_by_tx':
             emb_data = bq.load_embeddings()
 
             # Add words to the dictionary
@@ -194,17 +202,18 @@ class Features(object):
                 self.dictionary.add_merchant(merchant, embedding)
             print('Merchant vocab size: {0}'.format(len(self.dictionary.idx2merchant)))
 
+        # Load data and execute processing logic
         processing_start = time.time()
 
-        raw_train = bq.load_sequences(self.dataset_name, self.seq_len, 'train')
+        raw_train = bq.load_sequences(self.args.dataset_name, self.args.seq_len, 'train')
         self.prepare_data(raw_train, 'train')
         raw_train = None
 
-        raw_val = bq.load_sequences(self.dataset_name, self.seq_len, 'val')
+        raw_val = bq.load_sequences(self.args.dataset_name, self.args.seq_len, 'val')
         self.prepare_data(raw_val, 'val')
         raw_val = None
 
-        raw_test = bq.load_sequences(self.dataset_name, self.seq_len, 'test')
+        raw_test = bq.load_sequences(self.args.dataset_name, self.args.seq_len, 'test')
         self.prepare_data(raw_test, 'test')
         raw_test = None
 
@@ -231,14 +240,125 @@ class Features(object):
                 raise result.stderr
 
 
+    def stdscaler_fit(self, X):
+        self.s = np.std(X)
+        self.u = np.mean(X)
+        pass
+
+
+    def stdscaler_transform(self, X):
+        Xsc = ( X - self.u ) / self.s
+        X = None
+        return Xsc
+
+
+    def prepare_data(self, data, split):
+        # A structure to enable indexing a numpy matrix with column names.
+        user_col = self.feature_config['user_reference']['column_index']
+        merchant_col = self.feature_config['merchant_name']['column_index']
+        dow_col = self.feature_config['day_of_week']['column_index']
+        eod_col = self.feature_config['eighth_of_day']['column_index']
+        amount_col = self.feature_config['amount']['column_index']
+        cat_col = self.feature_config['sys_category']['column_index']
+
+        # Scale amount values
+        scaling_start = time.time()
+
+        if self.feature_config['amount']['enabled']==True:
+            if split=='train':
+                amount_seqs = data[:,amount_col]
+                amounts = [x for seq in amount_seqs for x in seq]
+                amount_seqs = None
+                self.stdscaler_fit(amounts)
+                amounts = None
+
+            for idx, seq in enumerate(data[:,amount_col]):
+                seq_sc = self.stdscaler_transform(seq)
+                data[idx, amount_col] = seq_sc
+
+        scaling_end = time.time()
+        scaling_duration = round(scaling_end - scaling_start, 1)
+        print('scaling time: {0}s'.format(scaling_duration))
+
+        # tokenize pre-concurrency
+        tokenization_start = time.time()
+
+        for row in data:
+            self.dictionary.add_user(row[user_col])
+            for merchant in row[merchant_col]:
+                self.dictionary.add_merchant(merchant)
+
+        tokenization_end = time.time()
+        tokenization_duration = round(tokenization_end - tokenization_start, 1)
+        print('tokenization time: {0}s'.format(tokenization_duration))
+
+        # write to be read by row.transform
+        path = os.path.join('tmp_data', 'dictionary')
+        with open(path, 'wb') as f:
+            pickle.dump(self.dictionary.__dict__, f)
+
+        # Divide data into one chunk per cpu core
+        cores = multi.cpu_count()
+        chunk_size = math.ceil(len(data) / cores)
+        print('Running {0} chunks of {1} rows'.format(cores, chunk_size))
+        chunks = []
+        for pnum in range(cores):
+            start_id = pnum * chunk_size
+            stop_id = start_id + chunk_size
+            chunks.append(data[start_id:stop_id])
+
+        # Free up RAM
+        data = None
+
+        # Process data concurrently, one process per cpu core.
+        # Not guaranteed to use all cores.
+        row = Row(self.args)
+        with multi.Pool(processes=cores) as pool:
+            processed_chucks = pool.map(row.transform, chunks)
+            pool.close()
+            pool.join()
+
+        # Free up RAM
+        chunks = None
+
+        # Flatten list of mutliprocess outputs
+        samples = [item for sublist in processed_chucks for item in sublist]
+
+        # Free up RAM
+        processed_chucks = None
+
+        # Write to disk
+        path = os.path.join(self.data_dir, split)
+        print('Writing {0} to {1}'.format(split, path))
+        with open(path, 'wb') as f:
+            pickle.dump(samples, f)
+
+        # Free up RAM
+        samples = None
+
+        return None
+
+
+class Row(object):
+    def __init__(self, args):
+        self.args = args
+
+        path = os.path.join('tmp_data', 'dictionary')
+        with open(path, 'rb') as dict:
+            self.dictionary = pickle.load(dict)
+
+        with open('feature_config.json', 'r') as config:
+            self.feature_config = json.load(config)
+
+
     def prepare_token_sequence(self, seq):
         sequence = np.append('<cls>', np.array(seq))
         target = sequence[-1]
         input = sequence[:-1]
 
         # Adding padding
-        if len(input) < self.seq_len:
-            input = np.append(input, np.repeat('<pad>', self.seq_len-len(input)))
+        if len(input) < self.args.seq_len:
+            input = np.append(input, np.repeat('<pad>', self.args.seq_len-len(input)))
 
         return input, target
 
@@ -249,8 +369,8 @@ class Features(object):
         input = sequence[:-1]
 
         # Adding padding
-        if len(input) < self.seq_len:
-            input = np.append(input, np.repeat(0, self.seq_len-len(input)))
+        if len(input) < self.args.seq_len:
+            input = np.append(input, np.repeat(0, self.args.seq_len-len(input)))
 
         return input, target
 
@@ -277,55 +397,44 @@ class Features(object):
         return torch.squeeze(torch.stack((sin, cos),1))
 
 
-    def stdscaler_fit(self, X):
-        self.s = np.std(X)
-        self.u = np.mean(X)
-        pass
-
-
-    def stdscaler_transform(self, X):
-        Xsc = ( X - self.u ) / self.s
-        X = None
-        return Xsc
-
-
-    def process_row(self, chunk):
+    def transform(self, chunk):
         # This fucntion executes once per process
         start_time = time.time()
 
         log_interval = 10000
         samples = []
-        enabled_features = {k:v for k,v in self.feature_set.items() if v['enabled']==True}
+        enabled_features = {k:v for k,v in self.feature_config.items() if v['enabled']==True}
         for index, row in enumerate(chunk):
             input_dict = {}
             target_dict = {}
             for feature, config in enabled_features.items():
-                feat_seq = row[self.schema_dict[feature]]
+                col_idx = self.feature_config[feature]['column_index']
+                feat_seq = row[col_idx]
 
                 if feature =='user_reference':
                     user = feat_seq
-                    input_dict[feature] = self.tensor(self.dictionary.user2idx[user])
+                    input_dict[feature] = self.tensor(self.dictionary['user2idx'][user])
 
                 if feature == 'merchant_name':
                     sequence, target = self.prepare_token_sequence(feat_seq)
                     merchant_ids = []
                     masks = []
                     for merchant in sequence:
-                        merchant_ids.append(self.dictionary.merchant2idx[merchant])
+                        merchant_ids.append(self.dictionary['merchant2idx'][merchant])
                         if merchant != '<pad>':
                             masks.append(1)
                         else:
                             masks.append(0)
                     input_dict[feature] = self.tensor(np.array(merchant_ids))
-                    target_dict[feature] = self.tensor(self.dictionary.merchant2idx[target])
+                    target_dict[feature] = self.tensor(self.dictionary['merchant2idx'][target])
 
                 if feature == 'sys_category':
                     sequence, target = self.prepare_token_sequence(feat_seq)
                     cat_ids = []
                     for cat in sequence:
-                        cat_ids.append(self.dictionary.cat2idx[cat])
+                        cat_ids.append(self.dictionary['cat2idx'][cat])
                     input_dict[feature] = self.tensor(cat_ids)
-                    target_dict[feature] = self.tensor(self.dictionary.cat2idx[target])
+                    target_dict[feature] = self.tensor(self.dictionary['cat2idx'][target])
 
                 if feature in ['day_of_week', 'eighth_of_day']:
                     sequence, target = self.prepare_numeric_sequence(feat_seq)
@@ -351,94 +460,13 @@ class Features(object):
         return samples
 
 
-    def prepare_data(self, data, split):
-        # A structure to enable indexing a numpy matrix with column names.
-        self.schema_dict = {'auth_ts': 0,
-                    'user_reference': 1,
-                    'merchant_name': 2,
-                    'day_of_week': 3,
-                    'eighth_of_day': 4,
-                    'amount': 5,
-                    'sys_category': 6,
-                    'auth_ts_seq': 7}
-
-        # Scale amount values
-        scaling_start = time.time()
-
-        if self.feature_set['amount']['enabled']==True:
-            if split=='train':
-                amount_seqs = data[:,self.schema_dict['amount']]
-                amounts = [x for seq in amount_seqs for x in seq]
-                amount_seqs = None
-                self.stdscaler_fit(amounts)
-                amounts = None
-
-            for idx, seq in enumerate(data[:,self.schema_dict['amount']]):
-                seq_sc = self.stdscaler_transform(seq)
-                data[idx, self.schema_dict['amount']] = seq_sc
-
-        scaling_end = time.time()
-        scaling_duration = round(scaling_end - scaling_start, 1)
-        print('scaling time: {0}s'.format(scaling_duration))
-
-        # tokenize pre-concurrency
-        tokenization_start = time.time()
-
-        for row in data:
-            self.dictionary.add_user(row[self.schema_dict['user_reference']])
-            for merchant in row[self.schema_dict['merchant_name']]:
-                self.dictionary.add_merchant(merchant)
-
-        tokenization_end = time.time()
-        tokenization_duration = round(tokenization_end - tokenization_start, 1)
-        print('tokenization time: {0}s'.format(tokenization_duration))
-
-        # Divide data into one chunk per cpu core
-        cores = multi.cpu_count()
-        chunk_size = math.ceil(len(data) / cores)
-        print('Running {0} chunks of {1} rows'.format(cores, chunk_size))
-        chunks = []
-        for pnum in range(cores):
-            start_id = pnum * chunk_size
-            stop_id = start_id + chunk_size
-            chunks.append(data[start_id:stop_id])
-
-        # Free up RAM
-        data = None
-
-        # Process data concurrently, one process per cpu core.
-        # Not guaranteed to use all cores.
-        with multi.Pool(processes=cores) as pool:
-            processed_chucks = pool.map(self.process_row, chunks)
-            pool.close()
-            pool.join()
-
-        # Free up RAM
-        chunks = None
-
-        # Flatten list of mutliprocess outputs
-        samples = [item for sublist in processed_chucks for item in sublist]
-
-        # Free up RAM
-        processed_chucks = None
-
-        # Write to disk
-        path = os.path.join(self.data_dir, split)
-        print('Writing {0} to {1}'.format(split, path))
-        with open(path, 'wb') as f:
-            pickle.dump(samples, f)
-
-        # Free up RAM
-        samples = None
-
-        return None
 
 
 if __name__ == '__main__':
 
     parser = ArgumentParser()
 
-    parser.add_argument('--data', type=str, default='merchant_seqs_by_tx',
+    parser.add_argument('--dataset_name', type=str, default='merchant_seqs_by_tx',
                         help='Name of input data')
     parser.add_argument('--seq_len', type=int, default=32,
                         help='sequence length')
@@ -459,18 +487,4 @@ if __name__ == '__main__':
 
     print(args)
 
-    feature_set = {'merchant_name':
-                        {'enabled': True},
-                    'user_reference':
-                        {'enabled': args.include_user_context},
-                    'eighth_of_day':
-                        {'enabled': args.include_eighth_of_day},
-                    'day_of_week':
-                        {'enabled': args.include_day_of_week},
-                    'amount':
-                        {'enabled': args.include_amount},
-                    'sys_category':
-                        {'enabled': args.include_sys_category}
-                    }
-
-    Features(args.data, args.seq_len, args.sample_size, feature_set)
+    Features(args)
