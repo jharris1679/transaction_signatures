6ba2d3d66ba3f958ea665649340fe415b106512d
jharris1679 <jdharris1357@gmail.com>
2020-05-07 08:40:40 -0400

    remove category and timestamp from queries

--- tx_model/app/queries/merchant_seqs_by_tx_train.sql
+++ tx_model/app/queries/merchant_seqs_by_tx_train.sql
@@ -22,9 +22,7 @@ select
   array_agg(lower(merchant_name) order by b.auth_ts desc limit {{seq_len}}) merchant_name,
   array_agg(cast(day_of_week as int64) order by b.auth_ts desc limit {{seq_len}}) day_of_week,
   array_agg(cast(eighth_of_day as int64) order by b.auth_ts desc limit {{seq_len}}) eighth_of_day,
-  array_agg(cast(amount as float64) order by b.auth_ts desc limit {{seq_len}}) amount,
-  array_agg(sys_category order by b.auth_ts desc limit {{seq_len}}) sys_category,
-  array_agg(b.auth_ts order by b.auth_ts desc limit {{seq_len}}) auth_ts_seq
+  array_agg(cast(amount as float64) order by b.auth_ts desc limit {{seq_len}}) amount
 from transactions a
 join
   (select
