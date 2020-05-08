d9de90753685d338a9de60d68d1548955d4e662b
jharris1679 <jdharris1357@gmail.com>
2020-05-08 12:40:38 -0400

    assert max seq count per user > 1 and len(set(chunk)) > 1

--- tx_model/app/exec_preprocessing.sh
+++ tx_model/app/exec_preprocessing.sh
@@ -7,5 +7,4 @@ python3 preprocess.py \
         --include_user_context \
         --include_eighth_of_day \
         --include_day_of_week \
-        --include_amount \
-        --include_sys_category
+        --include_amount
