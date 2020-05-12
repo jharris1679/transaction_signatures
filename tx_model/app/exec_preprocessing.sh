20941619e1dcdb98c6db7573c5543725c16caf43
jharris1679 <jdharris1357@gmail.com>
2020-05-12 10:29:11 -0400

    turn on pretrained embeddings

--- tx_model/app/exec_preprocessing.sh
+++ tx_model/app/exec_preprocessing.sh
@@ -4,6 +4,7 @@ SAMPLE_SIZE=$1
 
 python3 preprocess.py \
         --sample_size $SAMPLE_SIZE \
+        --use_pretrained_embeddings \
         --include_user_context \
         --include_eighth_of_day \
         --include_day_of_week \
